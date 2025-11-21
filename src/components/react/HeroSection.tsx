933af655d11165ba4fe68f372a3349823f618eef
Vinicius Gomes <vinigomes.0311@gmail.com>
2025-11-21 20:02:49 -0300

    feat: add Acumin Pro font files and update styles for improved typography

--- src/components/react/HeroSection.tsx
+++ src/components/react/HeroSection.tsx
@@ -4,8 +4,8 @@ import { Button } from "../ui/button";
 export default function HeroSection() {
   return (
     <div className="text-center mb-12 lg:mb-16">
-      <h1 className="text-6xl md:text-8xl lg:text-6xl font-black text-primary leading-none mb-8">
-        HISTÓRIAS VISUAIS PODEROSAS
+      <h1 className="text-6xl md:text-8xl lg:text-6xl text-primary leading-none mb-8">
+        Histórias visuais poderosas
       </h1>
       <Button>
         <ArrowDownIcon className="inline-block" />
