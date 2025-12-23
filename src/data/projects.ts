60f57d88df17ce75a1f62407b6e70ebeaeafccae
Vinicius Gomes <vinigomes.0311@gmail.com>
2025-12-23 12:57:38 -0300

    feat: update project items with new entries and media links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,58 +14,168 @@ export interface ProjectItem {
 
 export const projectItems: ProjectItem[] = [
   {
-    id: "urban-fairytale",
-    title: "URBAN FAIRYTALE",
-    description: "EVERYDAY SCENES TRANSFORMED INTO MYTHICAL STORIES",
-    subtitle: "HINTS OF MAGIC AMONG THE VEGGIE STANDS",
-    image: "/projects/first/two.jpg",
-    videos: [],
-    photos: [
-      { src: "/projects/first/two.jpg" },
-      { src: "/projects/first/two-alt1.jpg" },
-      { src: "/projects/first/two-alt2.jpg" },
+    id: "aftermovie",
+    title: "AFTERMOVIE",
+    description: "AFTERMOVIE - PROJECT DESCRIPTION",
+    subtitle: "AFTERMOVIE - PROJECT SUBTITLE",
+    image:
+      "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/Co%CC%81pia%20de%20Co%CC%81pia%20de%20Senhor%20Boteco-thumb.jpg",
+    videos: [
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/Co%CC%81pia%20de%20Co%CC%81pia%20de%20Senhor%20Boteco.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/Co%CC%81pia%20de%20Modular%20-%20Halloween.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/C%C3%B3pia%20de%20R_kylie_003.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/Dre%20Guazelli.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/MC%20Davi%20no%20Espa%C3%A7o%20Liv.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/REELS%204K.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/AFTERMOVIE/barreiraeobaskla.mp4",
+      },
     ],
+    photos: [],
   },
   {
-    id: "street-serenity",
-    title: "STREET SERENITY",
-    description: "EVERYDAY MOMENTS BECOME CINEMATIC FRAMES",
-    subtitle:
-      "SHADOWS AND LIGHT WEAVING A MODERN MYTH SOFTER THAN SILK THE CITY SPEAKS",
-    image: "/projects/first/one.jpg",
-    videos: [],
-    photos: [{ src: "/projects/first/one.jpg" }],
+    id: "chevrolet",
+    title: "CHEVROLET ",
+    description: "CHEVROLET  - PROJECT DESCRIPTION",
+    subtitle: "CHEVROLET  - PROJECT SUBTITLE",
+    image:
+      "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CHEVROLET%20/MOF_SHORT_SUBTITLE-thumb.jpg",
+    videos: [
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CHEVROLET%20/MOF_SHORT_SUBTITLE.mp4",
+      },
+    ],
+    photos: [],
   },
   {
-    id: "lost-in-the-crowd",
-    title: "LOST IN THE CROWD",
-    description: "SOLITUDE IN THE NOISE",
-    subtitle: "A CALM MOMENT AMONG COUNTLESS FACES",
-    image: "/projects/first/three.jpg",
-    videos: [],
-    photos: [
-      { src: "/projects/first/three.jpg" },
-      { src: "/projects/first/three-alt1.jpg" },
-      { src: "/projects/first/three-alt2.jpg" },
-      { src: "/projects/first/three-alt3.jpg" },
+    id: "corporativo",
+    title: "CORPORATIVO",
+    description: "CORPORATIVO - PROJECT DESCRIPTION",
+    subtitle: "CORPORATIVO - PROJECT SUBTITLE",
+    image:
+      "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/2025_Evento_G4_TRACTION_JOURNEY__lw-thumb.jpg",
+    videos: [
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/2025_Evento_G4_TRACTION_JOURNEY__lw.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/2025_G4_TRACTION_JOURNEY_MAR%C3%87O_PREVIA%20v2.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/AFTERMOVIE_V1.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/CELESTE_4K.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/CRIMINALPLAYER_DIA01_V2.mov",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/CRIMINALPLAYER_DIA02_V1.mov",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/Ecommerce%20Brasil%202022.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/FINAL_2.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/INAUGURAC%CC%A7A%CC%83O%20ESCRITO%CC%81RIO%20CASTROL%20-%202025%20.mov",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/INPACTO_2024_DIA_01_4K_MP4.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/LEO%20TRADER%20AFTERMOVIE.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/M300%2025.09%20-%20SAMEDAY%20.MOV",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/METODO%20300%20AFTERMOVIE_V3.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/MONTAGEM.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/RUGBAR_V3%20.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/SAMEDAY%20-%20Galaxy.mov",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/SAMEDAY%20-%20Imersa%CC%83o%20Orion%20.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/SAMEDAY_HULK.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/VIDEO01_MARCOSMALUF.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/outlet.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/CORPORATIVO/vert_asociedadev2.mp4",
+      },
     ],
+    photos: [],
   },
   {
-    id: "city-soul",
-    title: "CITY SOUL",
-    description: "MODERN-DAY MUSE, NAVIGATING URBAN LEGENDS",
-    subtitle: "BOLD STEPS THROUGH NEON MEMORIES THE CITY IS HER STAGE",
-    image: "/projects/first/four.jpg",
-    videos: [{ src: "/projects/first/video-bla.mp4" }],
-    photos: [
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
-      { src: "/projects/first/four.jpg" },
+    id: "fashion-film",
+    title: "FASHION FILM",
+    description: "FASHION FILM - PROJECT DESCRIPTION",
+    subtitle: "FASHION FILM - PROJECT SUBTITLE",
+    image:
+      "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/FASHION%20FILM/PIET_MOV-thumb.jpg",
+    videos: [
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/FASHION%20FILM/PIET_MOV.mov",
+      },
     ],
+    photos: [],
+  },
+  {
+    id: "heinz",
+    title: "HEINZ ",
+    description: "HEINZ  - PROJECT DESCRIPTION",
+    subtitle: "HEINZ  - PROJECT SUBTITLE",
+    image:
+      "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/HEINZ%20/EP01_SEM_VINHETA-thumb.jpg",
+    videos: [
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/HEINZ%20/EP01_SEM_VINHETA.mp4",
+      },
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/HEINZ%20/EP02_KRAFTHEINZ_V2.mp4",
+      },
+    ],
+    photos: [],
+  },
+  {
+    id: "infantil",
+    title: "INFANTIL ",
+    description: "INFANTIL  - PROJECT DESCRIPTION",
+    subtitle: "INFANTIL  - PROJECT SUBTITLE",
+    image:
+      "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/INFANTIL%20/MATTEO_VERTICAL%20-thumb.jpg",
+    videos: [
+      {
+        src: "https://r2-ungant.whitecommand.com/portfolio/projects_temporary/INFANTIL%20/MATTEO_VERTICAL%20.mp4",
+      },
+    ],
+    photos: [],
   },
 ];
