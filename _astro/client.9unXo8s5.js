3c06cb3317bfee56d33d6c96bc0fccc1c6cafb9a
viniciussantos45 <viniciussantos45@users.noreply.github.com>
2026-01-16 00:16:39 +0000

    deploy: f2ae4adffdf641b5c19bba5bbf2d88c6d3c850cf

--- _astro/client.9unXo8s5.js
+++ _astro/client.9unXo8s5.js
@@ -1,4 +1,4 @@
-import{a as s1,r as ia}from"./index.Bf1nT9TB.js";var ai={exports:{}},de={},ui={exports:{}},ei={};/**
+import{a as s1,r as ia}from"./index.WFquGv8Z.js";var ai={exports:{}},de={},ui={exports:{}},ei={};/**
  * @license React
  * scheduler.production.js
  *
---
+++ _astro/images-gallery.BSyNQsSz.js
@@ -0,0 +1,1 @@
+import{I as i}from"./images-gallery.BQ6h38sh.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.WFquGv8Z.js";/* empty css                      */export{i as default};
